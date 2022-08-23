f76b7af7a2fb5a44dfca2eb4ea8dada951177698
Michal Ziembicki <vipmikeus@gmail.com>
2022-08-23 10:53:28 -0400

    installing gulp

---
+++ wp-content/themes/themexy/gulpfile.babel.js
@@ -0,0 +1,23 @@
+// var gulp = require('gulp');
+
+
+// gulp.task('default', defaultTask);
+
+// function defaultTask(done){
+  //place code
+//   console.log('heyyy')
+//   done();
+// }
+
+// gulp.task('hello', function(done){
+//   console.log('hello');
+//   done();
+// })
+import gulp from 'gulp';
+
+export const hello = (done) => {
+  console.log('hello15');
+  done();
+}
+
+export default hello
